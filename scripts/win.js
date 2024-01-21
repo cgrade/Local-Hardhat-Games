52c84df4bfd650e23fc4fb8503619a78c88c80a5
cgrade <gradeisageek@gmail.com>
2024-01-21 21:14:44 +0100

    Game 2 Passed: Winner event emmited successfuly via a message call

--- scripts/win.js
+++ scripts/win.js
@@ -1,18 +1,20 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
+    await game.setX(25);
+    await game.setY(25);
     const tx = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
     const receipt = await tx.wait();
-    console.log(receipt);
+    console.log(receipt.events[0].event);
 }
 
 main()
